6e648fb8db29b1a8dae48546e3f691a88d3be380
Denys <omelchenko4562@gmail.com>
2024-02-02 13:07:45 +0200

    modified file Information.jsx

--- project sql+react/frontend/information/src/Components/Information/Information.jsx
+++ project sql+react/frontend/information/src/Components/Information/Information.jsx
@@ -4,19 +4,24 @@ import style from './Information.module.css'
 import { URL_PEOPLE_ID } from "../../constants/const";
 import ButtonBackClick from "../ButtonBackClick/ButtonBackClick";
 import Loader from "./Lazy/Loader";
-import { useDispatch} from "react-redux";
+import { useDispatch, useSelector} from "react-redux";
 import { add, remove } from "../../redux/action/action";
+import yellow from './img/yellow.svg'
+import white from './img/white.svg'
 
 const DataInfo = lazy(()=>import('./Lazy/DataInfo'))
 
 
 
 function Information() {
-  
+
+  const selector = useSelector(state=>state.data);
+
+
 // const [isTrue, SetIsTrue] = useState(false)
 const [dateInfo, setDateInfo] = useState([])
+const [isTrue, setIsTrue] = useState(null)
 const [errorInfo, setErrorInfo] = useState(null)
-
 
 
   const dispatch = useDispatch()
@@ -24,14 +29,11 @@ const [errorInfo, setErrorInfo] = useState(null)
   const { id1, id2 } = useParams();
 
   const navigate = useNavigate();
-
-
+ 
 
 const addUser = (info) => () => dispatch(add({id: info.id, name: info.name}));
 
 const removeUser = (info) => () => dispatch(remove({id: info.id, name: info.name}));
-
-
 
 
 function onClicking(e){
@@ -47,21 +49,30 @@ function req(url){
             setErrorInfo(true);
           });
     }
+  
     useEffect(() => {
-        req(`${URL_PEOPLE_ID}/${id1}/${id2}`)
-          .then((result) => {
-            if (result) {
-              setDateInfo(result);
-              setErrorInfo(false);
-            } else {
-              setErrorInfo(true);
-            }
-          })
-          .catch((err) => {
-            console.log(err);
-            setErrorInfo(true);
-          });
-      }, [id1, id2]);
+      req(`${URL_PEOPLE_ID}/${id1}/${id2}`)
+        .then((result) => {
+          if (result) {
+            setDateInfo(result);
+            setErrorInfo(false);
+    
+            const isTrue = result.some(
+              (info) => selector.items
+              .some((item) => item.id === info.id));
+
+
+            setIsTrue(isTrue);
+          } else {
+            setErrorInfo(false);
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+          setErrorInfo(true);
+        });
+    }, [id1, id2, selector]);
+    
 
   return (
     <>
@@ -77,12 +88,14 @@ function req(url){
                   className={style.img__focus}
                   alt="img" 
                   />
-
-          
-                <button onClick={addUser(info)} >ADD</button>
-                 <button onClick={removeUser(info)}>DELETE</button> 
-
-          
+            
+                  <img
+                    src={isTrue ? yellow : white}
+                    onClick={() => (isTrue ? removeUser(info)() : addUser(info)())}
+                    className={style.img}
+                   alt="img"
+                  />
+                   
 
 
                   <Suspense  fallback={<Loader/>}>
